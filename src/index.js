5cd674f4ae62a6644b68db55e92befdfdf883ed2
LauricellaAndrea <andrealauricella97@gmail.com>
2022-10-31 20:35:25 +0100

    add jsx-01

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import ReactDOM from "react-dom";
+import { Hello } from "./Hello" 
+
+const helloWorldElement = <Hello />;
+const root = document.getElementById("root");
+
+
+ReactDOM.render(helloWorldElement, root)
